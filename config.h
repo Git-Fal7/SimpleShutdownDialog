a83cea5cf1236c0d6789aa9aa289d888cdf6bd83
Git-Fal7 <61941069+Git-Fal7@users.noreply.github.com>
2022-01-04 19:02:01 +0300

    Update config.h

--- config.h
+++ config.h
@@ -4,11 +4,13 @@
 #define REBOOT_CMD "reboot"
 #define SUSPEND_CMD "systemctl suspend"
 #define HIBERNATE_CMD "systemctl hibernate"
+#define LOGOUT_CMD "loginctl terminate-session"
 
 // for a list of keys see gdk/gdkkeysyms.h
 #define POWEROFF_HOTKEY GDK_KEY_p
 #define REBOOT_HOTKEY GDK_KEY_r
 #define SUSPEND_HOTKEY GDK_KEY_s
 #define HIBERNATE_HOTKEY GDK_KEY_h
+#define LOGOUT_HOTKEY GDK_KEY_l
 #define EXIT_HOTKEY GDK_KEY_q
 
